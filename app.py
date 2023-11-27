7cd57cfaa98f1a7bc4902e8ca4aaadfdfc755da1
trungly11 <trung.ly1@pm.me>
2023-11-26 23:13:23 -0500

    all working now

--- app.py
+++ app.py
@@ -1,81 +1,80 @@
-# Import required libraries
-from flask import Flask, request, render_template, send_file
+# Flask application
+import io
 import json
-import csv
 import xml.etree.ElementTree as ET
+from flask import Flask, request, render_template, jsonify, make_response
+import pandas as pd
 
-# Initialize Flask app
+# Create a Flask app instance
 app = Flask(__name__)
 
-# Homepage to upload JSON file or provide JSON input
+# Route for rendering the index.html page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = make_response(render_template('index.html'))
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
 
-# Endpoint to handle JSON input
+# Route for converting JSON to XML or CSV
 @app.route('/convert', methods=['POST'])
 def convert():
-    json_data = request.files['json_file'] if 'json_file' in request.files else request.form.get('json_input')
+    """
+    Endpoint to handle JSON input and convert it to XML or CSV.
+    """
+    json_data = request.files.get('json_file') or request.form.get('json_input')
+    
+    if not json_data:
+        return jsonify({'error': 'No JSON input provided'}), 400
+
+    try:
+        json_object = json.loads(json_data)
+    except json.decoder.JSONDecodeError:
+        return jsonify({'error': 'Invalid JSON input'}), 400
+
     conversion_type = request.form.get('conversion_type')
 
-    # Convert JSON to XML
     if conversion_type == 'xml':
-        root = ET.Element("root")
-        convert_json_to_xml(json.loads(json_data), root)
-        xml_data = ET.tostring(root).decode('utf-8')
+        xml_data = convert_json_to_xml(json_object)
+        return jsonify({'output': xml_data})
 
-        return render_template('output.html', output=xml_data)
-
-    # Convert JSON to CSV
     elif conversion_type == 'csv':
-        csv_data = convert_json_to_csv(json.loads(json_data))
+        csv_data = convert_json_to_csv(json_object)
+        return jsonify({'output': csv_data})
 
-        return render_template('output.html', output=csv_data)
+    return jsonify({'error': 'Invalid conversion type'}), 400
 
-    return 'Invalid conversion type'
+# Function to convert JSON to XML
+def convert_json_to_xml(json_data):
+    """
+    Convert JSON to XML recursively.
+    """
+    root = ET.Element("root")
+    convert_json_to_xml_recursive(json_data, root)
+    xml_data = ET.tostring(root).decode('utf-8')
+    return xml_data
 
-# Convert JSON to XML recursively
-def convert_json_to_xml(json_data, parent):
+# Recursive helper function for converting JSON to XML
+def convert_json_to_xml_recursive(json_data, parent):
+    """
+    Recursive helper function for converting JSON to XML.
+    """
     if isinstance(json_data, dict):
         for tag, value in json_data.items():
             element = ET.SubElement(parent, tag)
-            convert_json_to_xml(value, element)
+            convert_json_to_xml_recursive(value, element)
     elif isinstance(json_data, list):
         for item in json_data:
-            element = ET.SubElement(parent, 'item')
-            convert_json_to_xml(item, element)
-    else:
-        parent.text = str(json_data)
+            element = ET.SubElement(parent, "item")
+            convert_json_to_xml_recursive(item, element)
 
-# Convert JSON to CSV
+# Function to convert JSON to CSV
 def convert_json_to_csv(json_data):
-    output = ''
-    keys = set()
+    """
+    Convert JSON to CSV.
+    """
+    df = pd.json_normalize(json_data)
+    csv_data = df.to_csv(index=False)
+    return csv_data
 
-    for item in json_data:
-        keys.update(item.keys())
-
-    writer = csv.DictWriter(output, fieldnames=keys)
-    writer.writeheader()
-    writer.writerows(json_data)
-
-    return output
-
-# Endpoint to download the converted file
-@app.route('/download', methods=['POST'])
-def download():
-    output = request.form.get('output')
-    conversion_type = request.form.get('conversion_type')
-
-    if conversion_type == 'xml':
-        filename = 'output.xml'
-        response = send_file(output, as_attachment=True, attachment_filename=filename, mimetype='application/xml')
-    elif conversion_type == 'csv':
-        filename = 'output.csv'
-        response = send_file(output, as_attachment=True, attachment_filename=filename, mimetype='text/csv')
-
-    return response
-
-# Main function to run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()(debug=True)
